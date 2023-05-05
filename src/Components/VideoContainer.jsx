fde281a8a5c62883531791fe564fac76e8f6e9e6
surya pratap <thakursuryapratap9@gmail.com>
2023-05-05 23:52:19 +0530

    updates

--- src/Components/VideoContainer.jsx
+++ src/Components/VideoContainer.jsx
@@ -4,8 +4,11 @@ const VideoContainer = (props) => {
     return (
         <div>
             <div className='h-96 max-sm:h-60 w-96 max-sm:w-70 rounded-3xl bg-teal-800 border flex items-center justify-center '>
-                <span className='text-white bg-slate-800 p-2 rounded-2xl'>
-                    {props.type[0].toUpperCase() + props.type.substring(1)}
+                <span className='text-white bg-slate-800 p-2 rounded-2xl flex gap-1 items-center'>
+                    <span>
+                        {props.type[0].toUpperCase() + props.type.substring(1)}
+                    </span>
+                    <iframe src="https://giphy.com/embed/XaejLonqk19jDugJbq" width="20" height="20" class="giphy-embed" allowFullScreen></iframe>
                 </span>
             </div>
         </div>
